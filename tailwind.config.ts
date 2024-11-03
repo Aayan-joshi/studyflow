a5c38c3a545d84383a9439c49f4a4fc6d614aff4
Aayan Joshi <aayan.joshi@hotmail.com>
2024-11-03 22:36:02 +0545

    implement tailwind.config.ts with added color palatte extension.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,27 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "accent-gradient": "linear-gradient(90deg, #77ACCF 100%, #5764D9 100%, #3756BC 100%)",
+        "accent-800": "#FFF1E6",
+        "dark" : {
+          100: "#000000",
+          200: "#0F1117",
+          300: "#151821",
+          400: "#212734",
+          500: "#3F4354",
+          "gradient": "linear-gradient(232deg, rgba(23, 28, 35, 0.41) 0%, rgba(19, 22, 28, 0.70) 100%)",
+        },
+        "light" : {
+          900: "#FFFFFF",
+          850: "#FDFDFD",
+          800: "#F4F6F8",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
+        },
+        fontFamily: {
+          "inter": "var(--font-inter)",
+        }
       },
     },
   },
