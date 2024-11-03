0a6eb20cb4c33bace127c2fdb6065461fcef9066
Aayan Joshi <aayan.joshi@hotmail.com>
2024-11-03 22:56:56 +0545

    Add tailwindcss details like fontFamily, boxShadow, borderRadius Change location of fonts from public folder to app/fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,13 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const inter = localFont({
+  src: "./fonts/InterVF.tff",
+  variable: "--font-inter",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.tff",
+  variable: "--font-space-grotesk",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
