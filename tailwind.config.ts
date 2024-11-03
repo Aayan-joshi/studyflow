00703bdfb2c26f0d778b5ee15144e70d156dd305
Aayan Joshi <aayan.joshi@hotmail.com>
2024-11-03 22:56:56 +0545

    Add tailwindcss details like fontFamily, boxShadow, borderRadius Change location of fonts from public folder to app/fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss";
 
+import tailwindcss_animate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -29,10 +32,33 @@ const config: Config = {
         },
         fontFamily: {
           "inter": "var(--font-inter)",
-        }
+          "spaceGrotesk": "var(--font-space-grotesk)",
+        },
+        borderRadius: {
+          "2": "8px",
+          "1.5": "6px",
+          lg: "var(--radius)",
+          md: "calc(var(--radius) - 2px)",
+          sm: "calc(var(--radius) - 4px)",
+        },
+        boxShadow: {
+          "light-100":
+              "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)",
+          "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+          "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+          "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+          "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+        },
+        backgroundImage: {
+          "auth-dark": 'url("/images/auth-dark.png")',
+          "auth-light": 'url("/images/auth-light.png")',
+        },
+        screens: {
+          xs: "420px",
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcss_animate, typography],
 };
 export default config;
