c1e27fa0e3791ad90511a4d9aee16c60115139e5
Aayan Joshi <aayan.joshi@hotmail.com>
2024-11-11 18:56:20 +0545

    Implement Github OAuth Signin and Signout functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
 import React from 'react';
+import {Toaster} from "@/components/ui/toaster";
+import {SessionProvider} from "next-auth/react";
+import {auth} from "@/auth";
 
 const inter = localFont({
   src: "fonts/InterVF.ttf",
@@ -19,20 +22,33 @@ export const metadata: Metadata = {
   description: "Study Flow : Your Personal Study Assistant",
 };
 
-export default function RootLayout({
+const RootLayout= async ({
                                      children,
                                    }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
+    const session = await auth()
+
   return (
       <html lang="en" suppressHydrationWarning>
+        <SessionProvider session={session}>
           <body
               className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
           >
-            <ThemeProvider attribute={"class"} defaultTheme={`system`} enableSystem disableTransitionOnChange>
+            <ThemeProvider
+                attribute={"class"}
+                defaultTheme={`system`}
+                enableSystem
+                disableTransitionOnChange
+            >
                 {children}
             </ThemeProvider>
+           <Toaster />
           </body>
+        </SessionProvider>
+
       </html>
   );
 }
+
+export default RootLayout;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,11 +1,39 @@
+'use client'
+
+import ROUTES from "@/constants/routes";
 import React from 'react'
 import {Button} from "@/components/ui/button";
 import Image from "next/image";
+import {toast} from "@/hooks/use-toast";
+import {signIn} from "next-auth/react";
 
 const SocialAuthForm = () => {
+
+    const ButtonClass = "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3.5 rounded-2"
+
+    const handleSignIn = async (provider: "github" | "google") => {
+        try {
+            // await signIn(provider)
+            await signIn(provider, {
+                callbackUrl: ROUTES.HOME,
+                redirect: false,
+            })
+        } catch (error) {
+            console.error(error)
+
+            toast({
+                title: "Sign In Failed",
+                description:
+                    error instanceof Error
+                    ? error.message
+                    : "An unexpected error occurred. Please try again.",
+                variant: "destructive",
+            })
+        }
+    }
     return (
         <div className={`mt-10 flex flex-wrap gap-2.5`}>
-            <Button className={`background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3.5 rounded-2`}>
+            <Button className={ButtonClass} onClick={()=> handleSignIn('github')}>
                 <Image
                     src={`/icons/github.svg`}
                     alt={`Github Logo`}
@@ -15,7 +43,7 @@ const SocialAuthForm = () => {
                 />
                 <span>Log In with Github</span>
             </Button>
-            <Button className={`background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 px-4 py-3.5 rounded-2`}>
+            <Button className={ButtonClass} onClick={()=> handleSignIn('google')}>
                 <Image
                     src={`/icons/google.svg`}
                     alt={`Google Logo`}
