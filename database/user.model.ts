2ed84f7d44a0d6c7814df0041e87f7917630476a
Aayan Joshi <aayan.joshi@hotmail.com>
2025-04-09 09:58:10 +0545

    feat(user): make user image field optional in user model

--- database/user.model.ts
+++ database/user.model.ts
@@ -5,7 +5,7 @@ export interface IUser {
     username: string;
     email: string;
     bio?: string;
-    image: string;
+    image?: string;
     Location?: string;
     portfolio?: string;
     reputation?: number;
@@ -17,7 +17,7 @@ const userSchema = new Schema(
         username: { type: String, required: true, unique: true },
         email: { type: String, required: true, unique: true },
         bio: { type: String },
-        image: { type: String, required: true, default: "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?ga=GA1.1.1101452655.1742955870&semt=ais_hybrid" },
+        image: { type: String },
         Location: { type: String },
         portfolio: { type: String },
         reputation: { type: Number, default: 0 },
